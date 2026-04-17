b85df5fed93cc3eaf10fe33cb86a8aca1f90c71d
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-04-17 10:17:35 -0600

    Se agrego la parte de dashboard junto con pequeñas funciones

--- client-admin/src/features/auth/store/authStore.js
+++ client-admin/src/features/auth/store/authStore.js
@@ -3,35 +3,50 @@ import { persist } from "zustand/middleware";
 import { login as loginRequest } from "../../../shared/api";
 
 export const useAuthStore = create(
-  persist((set, get) => ({
-    user: null,
-    token: null,
-    expiresAt: null,
-    loading: false,
-    error: null,
-    isAuthenticated: false,
+  persist(
+    (set, get) => ({
+      user: null,
+      token: null,
+      expiresAt: null,
+      loading: false,
+      error: null,
+      isAuthenticated: false,
 
-    login: async ({ emailOrUsername, password }) => {
-      try {
-        set({ loading: true, error: null });
-        const { data } = await loginRequest({ emailOrUsername, password });
-        console.log(data);
+      login: async ({ emailOrUsername, password }) => {
+        try {
+          set({ loading: true, error: null });
+          const { data } = await loginRequest({ emailOrUsername, password });
+          console.log(data);
 
+          set({
+            user: data.userDetails,
+            token: data.token,
+            expiresAt: data.expiresAt,
+            loading: false,
+            isAuthenticated: true,
+          });
 
+          return { success: true, error: null };
+        } catch (err) {
+          console.error("Login error:", err);
+          const message =
+            err.response?.data?.message || "Error de autenticación";
+          set({ error: message, loading: false });
+          return { success: false, error: message };
+        }
+      },
+
+      logout: () => {
         set({
-          user: data.userDetails,
-          token: data.token,
-          expiresAt: data.expiresAt,
+          user: null,
+          token: null,
+          expiresAt: null,
           loading: false,
+          error: null,
+          isAuthenticated: false,
         });
-
-        return { success: true, error: null };
-      } catch (err) {
-        console.error("Login error:", err);
-        const message = err.response?.data?.message || "Error de autenticación";
-        set({ error: message, loading: false });
-        return { success: false, error: message };
-      }
-    },
-  })),
+      },
+    }),
+    { name: "auth-Storage" },
+  ),
 );
